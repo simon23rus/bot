8de39f2d953547cea1a8388cc2c5c3e07c1a5e23
sergmiller <sergey.miller@phystech.edu>
2016-11-19 22:48:40 +0300

    add state funtions

---
+++ process_orders.py
@@ -0,0 +1,41 @@
+import telegram
+import logging
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+
+def bot_init(bot, update):
+    create_user(update.message.from_user.id)
+    bot.sendMessage(update.message.from_user.id, 'Дратути')
+    return 'start choosing'
+
+def bot_inqueue(bot, update):
+    wait_start_cook(update.message.from_user.id)
+    bot.sendMessage(update.message.from_user.id, 'Мы начали готовить ваш заказ.')
+    return 'start cook'
+
+def bot_start_cook(bot, update):
+    wait_end_cook(update.message.from_user.id)
+    bot.sendMessage(update.message.from_user.id, 'Ваш заказ готов.')
+    wait_for_alarm(update.message.from_user.id)
+    return 'end cook'
+
+def bot_alarm(time, bot, update):
+    def alarm(bot, update):
+        status = wait_end_order(update.message.from_user.id, time)
+        bot.sendMessage(update.message.from_user.id, 'Ваш заказ был приготовлен ' + str(time) +  ' минут назад.')
+        if status == True:
+            return 'end order'
+        else:
+            return 'save order'
+    return alarm
+
+def bot_end_order(bot, update):
+    bot.sendMessage(update.message.from_user.id, 'Спасибо за ваш заказ!')
+    return 'return'
+
+def bot_blacklist(bot, update):
+    return 'lost order'
+
+def bot_banned(bot, update):
+    return 'unbanned'
+
--- states.py
+++ states.py
@@ -1,7 +1,11 @@
+from handlers import *
+from process_orders import *
+
 class Node:
-    def __init__(self, name):
+    def __init__(self, name, feedback):
         self.map = {}
         self.name = name
+        self.feedback = feedback
 
     def add_adj(self, message, node):
         self.map[message] = node
@@ -10,34 +14,39 @@ class Node:
         return self.map.get(message, self.map.get(''))
 
 class Graph:
-
-    def __init__(self, cur_node_name):
+    def __init__(self, cur_node_name, bot, update, alarm_time = [5,10]):
         self.nodes = {}
+        self.bot = bot
+        self.update = update
 
-        init = self.add_node('init')
-	choose = self.add_node('choose')
-	make_order = self.add_node('make_order')
-	start_cook = self.add_node('start_cook')
-	end_cook = self.add_node('end_cook')
-	alarm1 = self.add_node('alarm1')
-	alarm2 = self.add_node('alarm2')
-	blacklist = self.add_node('blacklist')
-	banned = self.add_node('banned')
-	
-	self.add_edge(init, choose, 'start choosing')
-	self.add_edge(choose, make_order, 'verify order')
-	self.add_edge(make_order, start_cook, 'start cook')
-	self.add_edge(start_cook, end_cook, 'end cook')
-	self.add_edge(end_cook, choose, 'end order')
-	self.add_edge(alarm1, choose, 'end order')
-	self.add_edge(alarm2, choose, 'end order')
-	self.add_edge(end_cook, alarm1, 'save order')
-     	self.add_edge(alarm1, alarm2, 'save order')
-        self.add_edge(alarm2, blacklist, 'lost order')
-	self.add_edge(blacklist, banned, 'ban')
-	self.add_edge(blacklist, choose, 'decrease loyalty')
-     	self.add_edge(banned, choose, 'increase loyalty')
-	
+        init = self.add_node('init', bot_init)
+        choose = self.add_node('choose', bot_choose)
+        make_order = self.add_node('make_order', bot_make_order)
+        inqueue = self.add_node('inqueue', bot_inqueue)
+        start_cook = self.add_node('start_cook', bot_start_cook)
+        end_cook = self.add_node('end_cook', bot_end_cook)
+        alarm1 = self.add_node('alarm1', bot_alarm(alarm_time[0]))
+        alarm2 = self.add_node('alarm2', bot_alarm(alarm_time[1]))
+        blacklist = self.add_node('blacklist', bot_blacklist)
+        banned = self.add_node('banned', bot_banned)
+        end_order = self.add_node('end_order', bot_end_order)
+
+        self.add_edge(init, choose, 'start choosing')
+        self.add_edge(choose, make_order, 'verify order')
+        self.add_edge(make_order, inqueue, 'wait inqueue')
+        self.add_edge(inqueue, start_cook, 'start cook')
+        self.add_edge(start_cook, end_cook, 'end cook')
+        self.add_edge(end_cook, choose, 'end order')
+        self.add_edge(alarm1, end_order, 'end order')
+        self.add_edge(alarm2, end_order, 'end order')
+        self.add_edge(end_cook, alarm1, 'wait getting')
+        self.add_edge(alarm1, alarm2, 'save order')
+        self.add_edge(alarm2, blacklist, 'save order')
+        self.add_edge(blacklist, banned, 'ban')
+        self.add_edge(blacklist, choose, 'lost order')
+        self.add_edge(banned, choose, 'unbanned')
+        self.add_edge(end_order, choose, 'return')
+
         self.cur_node = self.nodes[cur_node_name]
 
     def add_node(self, name):
@@ -50,7 +59,8 @@ class Graph:
 
     def go(self, msg):
         self.cur_node = self.cur_node.go(msg)
-        return self.cur_node.name
+        next_msg = self.cur_node.feedback(self, self.bot, self.update)
+        return self.cur_node.name, next_msg
 
 
 

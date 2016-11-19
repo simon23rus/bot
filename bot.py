8abc43c30ac832d027793a58823c15abf9163c24
Semyon Fedotov <simon23rus@gmail.com>
2016-11-19 18:01:44 +0300

    bot up

--- bot.py
+++ bot.py
@@ -1,2 +1,14 @@
 import telegram
 from telegram.ext import Updater
+
+token = '290457805:AAEzZlFxuUTwTzJYzhK7UcLoRhKdbkEVers'
+
+bot = telegram.Bot(token=token)
+updater = Updater(token=token)
+
+updates = bot.getUpdates()
+
+
+
+def start(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")

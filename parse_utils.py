59a6791ffb5efa0963aff968a0b68c00b8c8abad
Mikhail Nemilov <xmanatee.one@gmail.com>
2016-11-19 23:07:48 +0300

    Parse utils

---
+++ parse_utils.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+from parse_rest.connection import register
+from parse_rest.datatypes import Object
+from parse_rest.user import User
+import parse_config as config
+import time
+
+
+class User(Object):
+    pass
+
+
+class Order(Object):
+    pass
+
+
+class Product(Object):
+    pass
+
+
+def create_user(telegram_id):
+    user = User()
+    user.telegramId = telegram_id
+    user.save()
+
+
+class MenuItem(object):
+    def __init__(self, menu_id, product):
+        self.menu_id = menu_id
+        self.name = product.name
+        self.price = product.price
+        self.parse_id = product.objectId
+
+    def to_unicode(self):
+        return u'{}. {} - {}'.format(self.menu_id, self.name, self.price)
+
+    def __str__(self):
+        return self.to_unicode()
+
+    def __repl__(self):
+        return self.to_unicode()
+
+
+class MenuOrder(object):
+    def __init__(self, parse_order):
+        self.parse_order = parse_order
+
+    def cancel(self):
+        self.parse_order.status = "CANCELLED"
+        self.parse_order.save()
+
+    def wait_status(self, status, wait_time=10**3):
+        passed_time = 0
+        while self.parse_order.status != status and passed_time < wait_time:
+            self.parse_order = Order.Query.get(objectId = self.parse_order.objectId)
+            time.sleep(1)
+            wait_time += 1
+        return self.parse_order.status == status
+
+
+class Menu(object):
+    def __init__(self):
+        products = Product.Query.all()
+        self.items = []
+        for menu_id, product in enumerate(products, start=1):
+            self.items.append(MenuItem(menu_id, product))
+
+    def text(self):
+        menu_string = u''
+        for item in self.items:
+            menu_string += item.to_unicode() + u'\n'
+        return menu_string
+
+    def place_order(self, telegram_id, menu_id):
+        item = next(item for item in self.items if item.menu_id == menu_id)
+        user = User.Query.get(telegramId=telegram_id)
+        product = Product.Query.get(objectId=item.parse_id)
+
+        order = Order()
+        order.status = "IN_PROCESS"
+        order.save()
+        order.relation('product').add(product)
+        order.relation('user').add(user)
+        order.save()
+        return MenuOrder(order)
+
+
+def init_parse():
+    register(config.application_key, config.rest_api_key, master_key=config.master_key)
+
+
+def test():
+    init_parse()
+    menu = Menu()
+    print(menu.text())
+    order = menu.place_order('Danchik', 1)
+    print(order)
+
+
+if __name__ == "__main__":
+    test()

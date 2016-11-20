1210ca74ea9f34a73da1d0a3a3a70f0eff754cba
manatee <xmanatee.one@gmail.com>
2016-11-20 05:59:53 +0300

    refactor

--- telegram_config.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-bot_token = '288566170:AAHoHXmbZAM9lCFKMFkYhNAdUuuYOhYgfsw'
-bot_token_1 = '290457805:AAEzZlFxuUTwTzJYzhK7UcLoRhKdbkEVers'
-bot_token_2 = '218979213:AAE7PuYbbQjUAqvAX8_n4FHccgPqjL4r8xU'

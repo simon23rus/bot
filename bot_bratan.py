a844a7a2b1311a88272e5dc623f93ce698636aeb
Semyon Fedotov <simon23rus@gmail.com>
2016-11-20 04:44:02 +0300

    work

--- bot_bratan.py
+++ bot_bratan.py
@@ -1,38 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Simple Bot to reply to Telegram messages
-# This program is dedicated to the public domain under the CC0 license.
-"""
-This Bot uses the Updater class to handle the bot.
-
-First, a few callback functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Example of a bot-user conversation using ConversationHandler.
-Send /start to initiate the conversation.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-import telegram
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
                           ConversationHandler)
 
 import logging
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
 
-logger = logging.getLogger(__name__)
+WORKING = 1
 
-CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
-
-reply_keyboard = [['Шаверма'],['Other Staff']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
 
 
 def facts_to_str(user_data):
@@ -44,90 +20,92 @@ def facts_to_str(user_data):
     return "\n".join(facts).join(['\n', '\n'])
 
 
-def start(bot, update):
-    update.message.reply_text(
-        "Выберите товар:",
-        reply_markup=markup)
+def start(bot, update, user_data):
+    user_data['graph'] = Graph(bot, update.message.from_user.id)
+    update.message.text = 'start'
 
-    return CHOOSING
+    return work(bot, update, user_data)
 
 
-def regular_choice(bot, update, user_data):
+def createKeyboard(keys):
+    keyboard = []
+    for ind in range(len(keys) // 2):
+        print(ind)
+        keyboard.append([keys[2 * ind], keys[2 * ind + 1]])
+    
+    if len(keys) % 2 == 1:
+        keyboard.append([keys[-1]])
+
+    return keyboard
+
+
+def work(bot, update, user_data):
     text = update.message.text
-    user_data['choice'] = text
-    update.message.reply_text('Ваш заказ: %s \n Готовы ли Вы подтвердить его?\n' % text.lower())
-
-    return TYPING_REPLY
-
-
-def received_information(bot, update, user_data):
-    text = update.message.text
-    category = user_data['choice']
-    user_data[category] = text
-    del user_data['choice']
-
-    update.message.reply_text("Прекрасно! Вы можете заказать снова", reply_markup=markup)
-
-    return CHOOSING
-
-
-def done(bot, update, user_data):
-    if 'choice' in user_data:
-        del user_data['choice']
-
-    update.message.reply_text("I learned these facts about you:"
-                              "%s"
-                              "Until next time!" % facts_to_str(user_data))
-
-    user_data.clear()
-    return ConversationHandler.END
-
+    while True:
+        message, keys = user_data['graph'].go(text)
+        if len(keys) == 0:
+            update.message.reply_text(message)
+            text = ''
+        else: 
+            keyboard = createKeyboard(keys)
+            markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+            update.message.reply_text(message,
+                                    reply_markup=markup)
+            break
+        
+    return WORKING
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
 
 
+def done(bot, update, user_data):
+  print('zazaz')
+  return ConversationHandler.END
+
+def create_arguments_for_regex(keys):
+    ans = []
+    for key in keys:   
+        ans.append('^' + key + '$')
+
+
+
+    return '|'.join(ans)
 def main():
     # Create the Updater and pass it your bot's token.
-  config = open('config', 'r+')
-  token = config.readline().split('=')[1][:-1]
-  config.close()
-  bot = telegram.Bot(token=token)
-  updater = Updater(token=token)
+    config = open('config', 'r+')
+    token = config.readline().split('=')[1][:-1]
+    config.close()
+    updater = Updater(token=token)
 
-  # Get the dispatcher to register handlers
-  dp = updater.dispatcher
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
 
-  # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
-  conv_handler = ConversationHandler(
-      entry_points=[CommandHandler('start', start)],
+    arguments = create_arguments_for_regex(keys)
+    print(arguments)
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
 
-      states={
-          CHOOSING: [RegexHandler('^(Шаверма|Other Staff)$',
-                                  regular_choice,
-                                  pass_user_data=True)],
+        states={
+            WORKING: [RegexHandler('(?!Done)', work, pass_user_data=True)]
+        },
 
-          TYPING_REPLY: [MessageHandler(Filters.text,
-                                        received_information,
-                                        pass_user_data=True),
-                         ],
-      },
+        fallbacks=[RegexHandler('^Done$', done)]
+    )
 
-      fallbacks=[RegexHandler('^Done$', done, pass_user_data=True)]
-  )
+    dp.add_handler(conv_handler)
 
-  dp.add_handler(conv_handler)
+    # log all errors
+    dp.add_error_handler(error)
 
-  # log all errors
-  dp.add_error_handler(error)
+    # Start the Bot
+    updater.start_polling()
 
-  # Start the Bot
-  updater.start_polling()
-
-  # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
-  # SIGTERM or SIGABRT. This should be used most of the time, since
-  # start_polling() is non-blocking and will stop the bot gracefully.
-  updater.idle()
+    # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
 
 
 if __name__ == '__main__':
---
+++ parse_config.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+application_key = 'fJS9qIlbGMHqYa6M0RGyhDrmHEcDSoR3w774QmD1'
+rest_api_key = 'qVBkssCAhwZSf2AploYE3IHsEfZIzjVMaFT3fImO'
+master_key = 'e1EEbURGMQ16BeLzknLdevPtgKN6BptGbqZDlTfN'
